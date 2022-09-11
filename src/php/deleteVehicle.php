5dfb64da5312a07d0c4fe390b485e0f9f67637d0
Wesley DuBose <wsd10205@gmail.com>
2022-09-10 21:11:20 -0400

    Create deleteVehicle.php

---
+++ src/php/deleteVehicle.php
@@ -0,0 +1,34 @@
+<?php
+// Allow Access-Control-Allow-Origin
+    header('Access-Control-Allow-Origin: *');
+
+// Server Connection Data
+    $servername = "localhost";
+    $username = "dubdee";
+    $password = "Mustang73!";
+    $dbname = "autonarrsDB";
+
+// Retrieving the ID of the Vehicle Being Removed from The Request URL
+    $deleteID = $_REQUEST["q"];
+
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+// SQL Statement
+  $sql = "DELETE FROM inventory WHERE id=$deleteID;";
+
+// SQL Validation
+    if ($conn->query($sql) === TRUE) {
+        header('Location: http://www.autonarrs.com/src/pages/viewInventory.html');
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    };
+
+// Close the connection to the Server
+    $conn->close();
+?>
