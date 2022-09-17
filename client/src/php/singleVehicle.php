645cc1ba42702e3693d388c6878955c807847089
Wesley DuBose <wsd10205@gmail.com>
2022-09-17 05:38:26 -0400

    Create singleVehicle.php

---
+++ client/src/php/singleVehicle.php
@@ -0,0 +1,51 @@
+<?php
+header("Content-Type: application/json");
+
+// Server Connection Data
+  $servername = "localhost";
+  $username = "dubosews";
+  $password = "dubosewsSQL!";
+  $dbname = "autonarrsDB";
+  $stockID = $_POST['viewVehicle'];
+
+// Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+// SQL Statement
+  $sql = "SELECT * FROM inventory WHERE id=$stockID";
+
+// SQL Validation
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    
+  }
+
+  $result = $conn->query($sql);
+
+
+  
+  
+  if ($result->num_rows > 0) {
+    $respArray = [];
+    // output data of each row
+    while($data = $result->fetch_assoc()) {
+      $respData = [ 'id' => $data["id"], 'year' => $data["year"], 'make' => $data["make"], 'model' => $data["model"], 'trim' => $data["trim"], 'mileage' => $data["mileage"], 'price' => $data["price"], 'description' => $data["description"]];
+      array_push($respArray, $respData);
+    }
+    <div>
+     $respArray
+    </div> $respArray;
+    echo $stockID;
+  } else {
+    echo "0 results";
+  }
+
+  
+
+$conn->close();
